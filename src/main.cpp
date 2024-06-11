8a36e7b851a274cf5b7ad5a222368a2d931e5ffb
rubenfin <rubenkfinnerud@gmail.com>
2024-06-11 16:43:47 +0200

    added execute + main

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,19 @@
 /*   By: rfinneru <rfinneru@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/06/09 14:50:29 by rfinneru      #+#    #+#                 */
-/*   Updated: 2024/06/09 14:58:27 by rfinneru      ########   odam.nl         */
+/*   Updated: 2024/06/11 16:42:53 by rfinneru      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "webserv.hpp"
+#include "../include/Webserv.hpp"
 
-int	main(int ac, char **av)
+int	main(int argc, char **argv)
 {
+	if (argc != 2)
+		return (1);
+	std::string filename = argv[1];
+	Webserv webserv(filename);
+	webserv.printParsing();
+	webserv.execute();
 	return (0);
 }
