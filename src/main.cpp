b74019ec62f30eb527afa7908394c9edd987b503
rubenfin <rubenkfinnerud@gmail.com>
2024-07-03 17:36:18 +0200

    added extra check for argc and argv

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jade-haa <jade-haa@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/06/09 14:50:29 by rfinneru      #+#    #+#                 */
-/*   Updated: 2024/07/03 16:36:39 by rfinneru      ########   odam.nl         */
+/*   Updated: 2024/07/03 16:46:39 by rfinneru      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@
 int	main(int argc, char **argv, char **env)
 {
 	if (argc != 2)
-		return (1);
+		return (std::cerr << "usage: ./webserv [configuration_file]" << std::endl,
+			EXIT_FAILURE);
 	std::string filename = argv[1];
-	HttpHandler test;
+	if (!checkIfFile(filename))
+		return (std::cerr << "The configuration filepath is not correct" << std::endl,
+			EXIT_FAILURE);
 	Webserv webserv(filename, env);
-	// webserv.printParsing();
 	webserv.execute();
-	return (0);
-} // ~HttpHandler;
+	return (EXIT_SUCCESS);
+}
