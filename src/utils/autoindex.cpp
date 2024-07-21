16846e609ace48d7a3cd44c38ea054800228d980
rubenfin <rubenkfinnerud@gmail.com>
2024-07-21 15:16:13 +0200

    changed it from opendir to stat so i could add last modified and size to autindex

--- src/utils/autoindex.cpp
+++ src/utils/autoindex.cpp
@@ -6,34 +6,64 @@
 /*   By: rfinneru <rfinneru@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/07/21 11:57:13 by rfinneru      #+#    #+#                 */
-/*   Updated: 2024/07/21 15:01:48 by rfinneru      ########   odam.nl         */
+/*   Updated: 2024/07/21 15:14:59 by rfinneru      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/Server.hpp"
 
-std::string Server::returnAutoIndex(std::string &uri)
-{
-    struct dirent *de;
+std::string Server::returnAutoIndex(std::string &uri) {
     std::string autoIndexFile;
-    std::string uriNoRoot;
+    std::string uriNoRoot = uri.substr(getRoot().size(), uri.size() - getRoot().size()) + "/";
     
-    uriNoRoot = uri.substr(getRoot().size(), uri.size() - getRoot().size()) + "/";
     logger.log(DEBUG, uriNoRoot);
+    
     DIR *dr = opendir(uri.c_str());
-    autoIndexFile += "<html>\n<body>";
-    autoIndexFile += "<b>Index of " + uriNoRoot;
-    autoIndexFile += "</b>\n<hr>\n";
-    if (dr == NULL)
-    {
+    if (dr == NULL) {
         logger.log(ERR, "Could not open directory in autoindex");
         throw NotFoundException();
     }
 
-    while ((de = readdir(dr)) != NULL) 
-        autoIndexFile += "<a href=\"" + uriNoRoot + (std::string)de->d_name +  "\">" + (std::string)de->d_name + "<//a><br>\r\n";
+    autoIndexFile += "<html>\n<head><title>Index of " + uriNoRoot + "</title></head>\n<body>";
+    autoIndexFile += "<h1>Index of " + uriNoRoot + "</h1>\n<hr>\n<pre>";
+    autoIndexFile += "<table>\n";
+    autoIndexFile += "<tr><th>Name</th><th>Last modified</th><th>Size</th></tr>\n";
 
-    autoIndexFile += "\n<hr>\n</body>\n</html>";
-    closedir(dr);   
-    return (autoIndexFile);    
+    struct dirent *entry;
+    struct stat statbuf;
+    std::string fullPath;
+
+    while ((entry = readdir(dr)) != NULL) {
+        fullPath = uri + "/" + entry->d_name;
+        
+        if (stat(fullPath.c_str(), &statbuf) == -1) {
+            continue;  // Skip if we can't get file info
+        }
+
+        std::string fileName = entry->d_name;
+        std::string fileType = S_ISDIR(statbuf.st_mode) ? "directory" : "file";
+        
+        // Format last modified time
+        char timeBuf[80];
+        strftime(timeBuf, sizeof(timeBuf), "%Y-%m-%d %H:%M:%S", localtime(&statbuf.st_mtime));
+        
+        // Format file size
+        std::stringstream fileSizeStream;
+        if (S_ISDIR(statbuf.st_mode)) {
+            fileSizeStream << "-";
+        } else {
+            fileSizeStream << statbuf.st_size;
+        }
+
+        autoIndexFile += "<tr>";
+        autoIndexFile += "<td><a href=\"" + uriNoRoot + fileName + "\">" + fileName + "</a></td>";
+        autoIndexFile += "<td>" + std::string(timeBuf) + "</td>";
+        autoIndexFile += "<td align=\"right\">" + fileSizeStream.str() + "</td>";
+        autoIndexFile += "</tr>\n";
+    }
+
+    autoIndexFile += "</table>\n</pre>\n<hr>\n</body>\n</html>";
+    closedir(dr);
+
+    return autoIndexFile;
 }
